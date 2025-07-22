7df0ae9760e2472283acf7f22d470de971165d9a
Sanjay Doppalapudi <doppalapudisanjay@gmail.com>
2025-07-22 19:04:10 -0400

    Updated 2.0

--- ToDoList.py
+++ ToDoList.py
@@ -3,12 +3,15 @@ from tkinter import *
 def add():
     new = entry.get()
     listbox.insert(END, new)
+    entry.delete(0, END)
 def delete_selected():
     sel = listbox.curselection()
     if sel:
         listbox.delete(sel[0])
+        entry.delete(0, END)
 def delete():
     listbox.delete(0,END)
+    entry.delete(0, END)
 
 window = Tk()
 window.title("ToDo")
@@ -16,10 +19,10 @@ entry = Entry(window)
 listbox = Listbox(window)
 listbox.pack()
 entry.pack()
-button = Button(window, text="Add", command=  add)
-button1 = Button(window, text="Delete Selected", command=  delete_selected)
-button2 = Button(window, text="Delete", command=  delete)
-button.pack()
-button1.pack()
-button2.pack()
+button = Button(window, text="Add", command=  add, width=12, height=1)
+button1 = Button(window, text="Delete Selected", command=  delete_selected, width=12, height=1)
+button2 = Button(window, text="Delete", command=  delete, width=12, height=1)
+button.pack(side=LEFT, pady=10, padx=10)
+button1.pack(side=LEFT, pady=10, padx=10)
+button2.pack(side=LEFT, pady=10, padx=10)
 window.mainloop()
