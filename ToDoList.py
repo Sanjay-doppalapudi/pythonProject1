7ffff5756e944b8cd7c6e24070ddf928ccc71c5c
Sanjay Doppalapudi <doppalapudisanjay@gmail.com>
2025-07-22 18:52:21 -0400

    Updated

--- ToDoList.py
+++ ToDoList.py
@@ -1,15 +1,20 @@
 from tkinter import *
 
 def add():
-    pass
+    new = entry.get()
+    listbox.insert(END, new)
 def delete_selected():
-    pass
+    sel = listbox.curselection()
+    if sel:
+        listbox.delete(sel[0])
 def delete():
-    pass
+    listbox.delete(0,END)
 
 window = Tk()
 window.title("ToDo")
 entry = Entry(window)
+listbox = Listbox(window)
+listbox.pack()
 entry.pack()
 button = Button(window, text="Add", command=  add)
 button1 = Button(window, text="Delete Selected", command=  delete_selected)
